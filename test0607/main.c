788648a9fb12663d492a739cc32593d81cc1d8b3
20161104597 <964347085@qq.com>
2017-06-07 08:46:59 +0800

    Initial Commit

---
+++ test0607/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test0607
+//
+//  Created by SantaClousJR on 17/6/7.
+//  Copyright © 2017年 SantaClousJR. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
